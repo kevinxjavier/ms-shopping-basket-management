0e6710aca72b89c9442006e9809d9cfc2fe00f66
Kevin Piña <kevin_pina@outlook.com>
2024-10-16 15:14:22 +0200

    Modified validation of Token and matchers

--- ms-shopping-basket-management-rest/src/main/java/com/kevinpina/shopping/management/rest/v1/config/JwtRequestFilter.java
+++ ms-shopping-basket-management-rest/src/main/java/com/kevinpina/shopping/management/rest/v1/config/JwtRequestFilter.java
@@ -1,4 +1,48 @@
 package com.kevinpina.shopping.management.rest.v1.config;
 
-public class JwtRequestFilter {
+import com.kevinpina.shopping.management.domain.exception.InvalidTokenException;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+	@Value("${security.jwt.secret-key}")
+	private String secretKey;
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+
+		String authorizationHeader = request.getHeader("Authorization".toLowerCase());
+		String username = null;
+
+		if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+			String token = authorizationHeader.substring(7);
+			try {
+				Claims claims = Jwts.parser()
+						.setSigningKey(secretKey)
+						.parseClaimsJws(token)
+						.getBody();
+				username = claims.getSubject();
+			} catch (Exception e) {
+				response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+				return;
+			}
+		} else {
+			response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+			return;
+		}
+
+		filterChain.doFilter(request, response);
+	}
 }
--- ms-shopping-basket-management-rest/src/main/java/com/kevinpina/shopping/management/rest/v1/config/SecurityConfig.java
+++ ms-shopping-basket-management-rest/src/main/java/com/kevinpina/shopping/management/rest/v1/config/SecurityConfig.java
@@ -1,4 +1,34 @@
 package com.kevinpina.shopping.management.rest.v1.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+
+	@Autowired
+	private JwtRequestFilter jwtRequestFilter;
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+/*		http.csrf(AbstractHttpConfigurer::disable) // csrf -> csrf.disable()
+				.authorizeHttpRequests(auth -> auth
+						.requestMatchers( "/public/**", "/health").permitAll()  // Allowed paths
+						.requestMatchers(HttpMethod.POST, "/api/**").permitAll()  // Allowed paths
+						.anyRequest().authenticated()  // Other authentication required
+				).csrf(csrf -> csrf
+						.ignoringRequestMatchers("/**"))
+				.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);  // JWT filter apply
+*/
+		http.authorizeHttpRequests(authorize -> authorize
+				.requestMatchers("/**").permitAll()
+		).csrf(csrf -> csrf
+				.ignoringRequestMatchers("/**"));
+		return http.build();
+	}
+
 }
+
